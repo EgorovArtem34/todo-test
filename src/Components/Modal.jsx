32c8c7d05d7f16f5182e8037514da9f6e73737e8
Artem Egorov <artegtor@gmail.com>
2023-04-10 10:38:02 +0400

    add src/Components/Modal.jsx

---
+++ src/Components/Modal.jsx
@@ -0,0 +1,12 @@
+import { useSelector } from 'react-redux';
+import { getModal } from './ModalComponent/index.js';
+
+const Modal = () => {
+  const { type } = useSelector((state) => state.modalsSlice);
+  if (type === null) {
+    return null;
+  }
+  const CurrentModal = getModal(type);
+  return <CurrentModal />
+};
+export default Modal;
